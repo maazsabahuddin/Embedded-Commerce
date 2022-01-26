a81f2c406bcf3c54110fc62ea663ca4e65e5cbef
root <root@DESKTOP-12EHMJQ.localdomain>
2022-01-26 11:25:29 +0500

    intial commit project initializatoin embedded commere

---
+++ ec/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ec.users.models import User
+from ec.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
